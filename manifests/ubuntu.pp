d4053f1df05aa3a2de501782c63b16fe2f3dabdb
Anthony Worrall <anthony.worrall@reading.ac.uk>
2018-09-12 12:11:47 +0100

    updated ubuntu.pp

--- manifests/ubuntu.pp
+++ manifests/ubuntu.pp
@@ -2,15 +2,16 @@
 # Installs NVIDIA GPU drivers on Ubuntu.
 # Desired version of the driver is passed as parameter.
 #
-class nvidia::ubuntu (Variant[Numeric, Enum['latest','recommended']] $version = 'latest') {
+class nvidia::ubuntu (
+  Variant[Numeric, Enum['latest','recommended']] $version = 'latest'
+) {
   case $version {
     'latest': {
       # Automatically finds the newest nvidia package
-      $nvidia_version = $facts['nvidia-latest']
-      $pkg_name = "nvidia-${facts['nvidia-latest']"
+      $pkg_name = "nvidia-${::facts['nvidia-latest']}"
     }
     'recommended': {
-      $pkg_name = $facts['nvidia-recommended']
+      $pkg_name = $::facts['nvidia-recommended']
     }
     default: {
       $nvidia_version = $version
